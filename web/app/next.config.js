3aec5507144ef5d616d73f0ec9552ba0c69a4cd1
Jan Lauber <jan.lauber@protonmail.ch>
2022-09-19 21:18:47 +0200

    add precommit
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- web/app/next.config.js
+++ web/app/next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  trailingSlash: true,
+  nextConfig,
+  publicRuntimeConfig: {
+    ENV_API_URI: process.env.ENV_API_URI,
+  },
+  images: {
+    // loader: "custom",
+    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'images.unsplash.com'],
+    // nextImageExportOptimizer: {
+    //   imageFolderPath: "public/images",
+    //   exportFolderPath: "out",
+    //   quality: 75,
+    // },
+  },
+  env: {
+    storePicturesInWEBP: true,
+  },
+}
