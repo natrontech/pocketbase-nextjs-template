d3bb48da903aaa66ce8c48cf352ec4283862943a
Jan Lauber <jan.lauber@protonmail.ch>
2022-09-20 13:33:07 +0200

    add monolith func
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- cmd/pocketbase-nextjs-template/main.go
+++ cmd/pocketbase-nextjs-template/main.go
@@ -4,11 +4,19 @@ import (
 	"log"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
 	app := pocketbase.NewWithConfig(pocketbase.Config{
 		DefaultDataDir: "/data",
+	})
+
+	// will serve the nextjs app from /web/app
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.Static("/", "web")
+
+		return nil
 	})
 
 	if err := app.Start(); err != nil {
